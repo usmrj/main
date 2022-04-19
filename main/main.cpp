fbdcccdf1d42fbdc640f42d0d03408190c9c538b
wojsyn <cajgero@gmail.com>
2022-04-19 14:33:48 +0200

    fisrt

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,16 @@
 ﻿#include <iostream>
 
+struct Person {
+    char* name;
+    char* surname;
+    Person* next;
+};
+
+namespace List {
+
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
 }
 
