e4392a1fdd6c495dbcc2db31bc9f356a9b4d8cd1
wojsyn <cajgero@gmail.com>
2022-04-23 12:34:14 +0200

    add

--- main/main.cpp
+++ main/main.cpp
@@ -21,11 +21,12 @@ int main()
     bool go_on = 1;
     while (go_on){
         unsigned int wybor;
-        std::cout << "Menu:\n1. Dodaj\n2. Wyswietl\n3. Rozmiar \n4. Usun \n0. Wyjscie";
+        std::cout << "Menu:\n1. Dodaj\n2. Wyswietl\n3. Rozmiar \n4. Usun \n0. Wyjscie\nTwoj wybor:";
         std::cin >> wybor;
         switch (wybor)
         {
         case 0:
+            go_on = 0;
             break;
         case 1:
             List::Add(List::Create());
@@ -34,7 +35,7 @@ int main()
             List::Show();
             break;
         case 3:
-            std::cout << "rozmiar osoby:" << List::Size();
+            std::cout << "rozmiar osoby:" << List::Size()<<std::endl;
             break;
         case 4:
             List::Remove();
@@ -42,16 +43,44 @@ int main()
         default:
             break;
         }
+        //system("cls");
     }
 
 }
 
 namespace List {
     Person* Create() {
+        char buff[512];
+        unsigned int str_length;
+        Person* osoba = new Person;
+        std::cout << "Wprowadz imie osoby: ";
+        std::cin >> buff;
+        str_length = std::strlen(buff);
+        osoba->name = new char[str_length + 1];;
+        //std::memcpy(osoba->name, buff, str_length * sizeof(char)); //nie dziala
+        std::cout << std::strlen(osoba->name);
+        std::cout << "Wprowadz nazwisko osoby: ";
+        std::cin >> buff;
+        str_length = std::strlen(buff);
+        osoba->surname = new char[str_length + 1];
+        //std::memcpy(static_cast<void*>(osoba->surname), buff, str_length * sizeof(char)); //nie dziala
+        std::cout<<*(osoba->surname+str_length);
+        std::cout<<osoba->surname;
+        osoba->next = 0;
         return 0;
     }
     void Add(Person* stara) {
-
+        if (root == 0) {
+            root = stara;
+        }
+        else{
+            Person* var;
+            var = root;
+            while (var->next!=0){
+                var = var->next;
+            }
+            var->next = stara;
+        }
     }
     void Show() {
 
